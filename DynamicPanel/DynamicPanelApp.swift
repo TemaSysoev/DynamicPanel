96355206519a0b0dcdfdbebc2f8cf3cc6e69c28f
TemaSysoevMBP <artyom.sysoev@gmail.com>
2021-02-22 15:43:37 +0300

    Initial Commit

---
+++ DynamicPanel/DynamicPanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicPanelApp.swift
+//  DynamicPanel
+//
+//  Created by Tema Sysoev on 22.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct DynamicPanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
